57ee6be0eee4ade1dbb7c043337c9fa59ad30ad3
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-09-30 14:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -31,11 +31,17 @@ const lookup = {
   ",": ",",
 };
 
-function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+function rot13(str) {
+    return str.replace(/[A-Z]/g, (match) => {
+        const charCode = match.charCodeAt(0);
+        const offset = charCode < 78 ? 13 : -13;
+        return String.fromCharCode(charCode + offset);
+    });
+}
+// Your Result goes here
   // Only change code below this line
 
-  return; //return decodedArr
+   //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
